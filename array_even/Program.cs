9a466ba085b81b1f224476d21dd241004414e2ce
Olua <a8rill@mail.ru>
2023-03-02 21:06:05 +0300

    added .gitignore file and array_even project

---
+++ array_even/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте массив заполненный случайными положительными 
+// трёхзначными числами. Напишите программу, которая 
+// покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+int[] array = GetArray(4, 100, 999);
+Console.WriteLine($"Массив: [{String.Join(";", array)}]");
+Console.WriteLine($"Количество чётных чисел:{GetEven( array)}");
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+int[] result = new int[size]; 
+for (int i = 0; i < result.Length; i++) 
+{
+result[i] = new Random().Next(minValue, maxValue + 1);
+}
+return result; 
+}
+
+int GetEven( int [] number )
+{ int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+      if (array[i] % 2 == 0)
+    count++;
+    }
+  return count; 
+}
