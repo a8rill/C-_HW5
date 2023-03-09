7dfacb1bee203cf20bd6c0255025a01cdf82b65c
Olua <a8rill@mail.ru>
2023-03-09 20:33:04 +0300

    added fixes sum_uneven project

--- sum_uneven/Program.cs
+++ sum_uneven/Program.cs
@@ -4,7 +4,7 @@
 // [3, 7, 23, 12] -> 19
 
 // [-4, -6, 89, 6] -> 0
-int[] array = GetArray(8, -100, 100);
+int[] array = GetArray(10, -10, 10);
 Console.WriteLine($"Массив: [{String.Join(";", array)}]");
 
 int[] GetArray(int size, int minValue, int maxValue)
@@ -18,11 +18,12 @@ return result;
 }
 
 
-int sumUneven = 0; 
 
 int size = array.Length;
+int sumUneven = 0; 
 for (int i = 1; i < size; i+=2)
 {
+Console.WriteLine($"array [{i}]={array[i]}"); 
 sumUneven += array[i];
 }
 Console.WriteLine($"Сумма элеменотов стощих на  нечетных позициях:{sumUneven}");
